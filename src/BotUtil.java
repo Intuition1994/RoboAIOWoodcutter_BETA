c2d8fc30f69c9027eaa754f2be48981d78904424
Tarolion <jt13602gaming@gmail.com>
2015-03-27 21:57:45 +0000

    ReUploading Beta Code
    Accidently deleted it while uploading Release :P Moved instead of Copied
    xD

---
+++ src/BotUtil.java
@@ -0,0 +1,251 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by jt13602 on 24/03/2015.
+ */
+public class BotUtil
+{
+    public static String version = "0.41";
+
+    public static RoboWoodcutter main;
+    public static PaintHandler paintHandler;
+
+    public static boolean BOT_IS_RUNNING        = false;
+    public static boolean BANK_LOGS             = false;
+    public static boolean PICKUP_ORTS           = false;
+    public static boolean InBankArea            = false;
+    public static PossibleLogs CHOSENLOGS       = PossibleLogs.NORMAL;
+    public static Banks CHOSENBANK              = Banks.None;
+    public static BotState BOTSTATE             = BotState.CHECKING;
+
+    public static int[] bankIDs             = new int[] { 2012, 2015, 2019, 4456, 4458, 4457, 4459 };
+    public static String[] bankNames           = new String[] { "Banker" };
+
+    // region [ Enumerators ]...
+    //
+    public static enum BotState
+    {
+        CHECKING,
+        CHOPPING_TREE,
+        BANKING_LOGS,
+        TRAVELING_TO_TREE,
+        TRAVELING_TO_BANK,
+        DROPPING_LOGS
+    }
+
+    public static enum PossibleLogs
+    {
+        NORMAL,
+        OAK,
+        WILLOW,
+        MAPLE,
+        YEW,
+        MAGIC,
+        ELDER,
+        CRYSTAL,
+        IVY
+    }
+
+    public static enum Banks
+    {
+        None,
+        Grand_Exchange,
+        Seers_Village,
+        Draynor,
+        Clan_Camp
+    }
+    //
+    //endregion
+
+    // region [ GUI Specific Methods ]...
+    //
+    public static boolean CanBankLogsHere(PossibleLogs log, Banks bank)
+    {
+        if( bank == Banks.None ) return true;
+
+        switch(log)
+        {
+            case NORMAL:
+                //region Normal Logs Banks
+                switch( bank )
+                {
+                    case Grand_Exchange:
+                        return true;
+                    default:
+                        return false;
+                }
+                //endregion
+
+            //region NOT YET ENABLED...
+//            case OAK:
+                //region OAK Logs Banks
+//                switch( bank )
+//                {
+//                    case Clan_Camp:
+//                        return true;
+//                    case Draynor:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case WILLOW:
+                //region WILLOW Logs Banks
+//                switch( bank )
+//                {
+//                    case Draynor:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+//                //endregion
+//            case MAPLE:
+                //region MAPLE Logs Banks
+//                switch( bank )
+//                {
+//                    case Seers_Village:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case YEW:
+                //region YEW Logs Banks
+//                switch( bank )
+//                {
+//                    case Grand_Exchange:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case MAGIC:
+                //region MAGIC Logs Banks
+//                switch( bank )
+//                {
+//                    case Seers_Village:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case ELDER:
+                //region ELDER Logs Banks
+//                switch( bank )
+//                {
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case CRYSTAL:
+                //region CRYSTAL Logs Banks
+//                switch( bank )
+//                {
+//                    default:
+//                        return false;
+//                }
+                //endregion
+//            case IVY:
+                //region IVY Banks
+//                switch( bank )
+//                {
+//                    case Grand_Exchange:
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+                //endregion
+            //endregion
+
+
+            default:
+                return false;
+        }
+    }
+    //
+    // endregion
+
+    // region [ Utility Methods ]...
+    //
+    public static void WriteMessage(String message)
+    {
+        System.out.println("[RoboAIOWoodcutter]: " + message);
+    }
+
+    public static String milliSecondsToString(long millis)
+    {
+        int secs = (int)(millis/1000);
+        int mins = secs/60;
+        int hours = mins/60;
+
+        String hrsString = "";
+        String mnsString = "";
+        String scsString = "";
+
+        if( hours < 10 )
+        {
+            hrsString = "0"+hours;
+        }
+        else
+        {
+            hrsString = ""+hours;
+        }
+
+        if( (mins%60) < 10 )
+        {
+            mnsString = "0"+(mins%60);
+        }
+        else
+        {
+            mnsString = ""+(mins%60);
+        }
+
+        if( (secs%60) < 10 )
+        {
+            scsString = "0"+(secs%60);
+        }
+        else
+        {
+            scsString = ""+(secs%60);
+        }
+
+        return hrsString+":"+mnsString+":"+scsString;
+    }
+
+    public static String GetBotStateString()
+    {
+        String state = BotUtil.BOTSTATE.toString();
+        String[] parts = state.split("_");
+        String output = "";
+
+        for(String s : parts)
+        {
+            output += s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase() + " ";
+        }
+
+        return output;
+    }
+
+    public static Image getImage(String url)
+    {
+        try
+        {
+            return ImageIO.read(new URL(url));
+        }
+        catch(IOException e)
+        {
+            return null;
+        }
+    }
+
+    public static Image ScaledImage(Image img, int scaleFactor)
+    {
+        Image scaledImage = img.getScaledInstance((int)(img.getWidth(null) * ((float)scaleFactor / 100f)), (int)(img.getHeight(null) * ((float)scaleFactor / 100f)), Image.SCALE_SMOOTH);
+        return scaledImage;
+    }
+    //
+    // endregion
+}
---
+++ src/CutIvy.java
@@ -0,0 +1,184 @@
+import com.epicbot.api.concurrent.Task;
+import com.epicbot.api.concurrent.node.Node;
+import com.epicbot.api.rs3.methods.Walking;
+import com.epicbot.api.rs3.methods.interactive.Players;
+import com.epicbot.api.rs3.methods.node.SceneEntities;
+import com.epicbot.api.rs3.methods.tab.inventory.Inventory;
+import com.epicbot.api.rs3.methods.widget.Bank;
+import com.epicbot.api.rs3.methods.widget.Camera;
+import com.epicbot.api.rs3.wrappers.Area;
+import com.epicbot.api.rs3.wrappers.Tile;
+import com.epicbot.api.rs3.wrappers.map.TilePath;
+import com.epicbot.api.rs3.wrappers.node.Item;
+import com.epicbot.api.rs3.wrappers.node.SceneObject;
+import com.epicbot.api.util.Random;
+import com.epicbot.api.util.Time;
+import javafx.scene.Scene;
+
+/**
+ * Created by jt13602 on 27/03/2015.
+ */
+public class CutIvy extends Node implements Task
+{
+
+    public BotUtil.PossibleLogs myLogs = BotUtil.PossibleLogs.IVY;
+    public String ivyName = "Ivy";      // When cutting Ivy we will use the name only instead of ID's
+    private SceneObject ivyToCut = null;
+    private String actionToUse = "Chop";
+
+    // NOTE: In this particular Case, Ivy does not use any of this data except the Tree Area. Since it does not bank, the bot aims
+    // to maintain cutting Ivy in the same area.
+    public Area ivyAreaToUse = null;
+
+    private boolean _started = false;
+    private boolean started = false;
+
+    //region [ Ivy Chopping Area's ]...
+
+    public final Area VarrockCastleArea = new Area(
+            new Tile(3201, 3507, 0),
+            new Tile(3208, 3497, 0),
+            new Tile(3221, 3497, 0),
+            new Tile(3227, 3506, 0)
+    );
+
+    private final Area FaladorSouthArea = new Area(
+            new Tile(3025, 3329, 0),
+            new Tile(3025, 3318, 0),
+            new Tile(3062, 3318, 0),
+            new Tile(3062, 3331, 0)
+    );
+
+    //endregion
+
+    // --------------------------
+    //  Task Execution condition
+    // --------------------------
+    @Override
+    public boolean shouldExecute()
+    {
+        boolean retVal = false;
+
+        if( !BotUtil.BOT_IS_RUNNING ) return false;
+
+        if( BotUtil.CHOSENLOGS == myLogs )
+        {
+            if( Players.getLocal() != null )
+            {
+                retVal = true;
+                started = true;
+            }
+        }
+
+        return retVal;
+    }
+
+    @Override
+    public void run()
+    {
+        if( _started != started )
+        {
+            _started = started;
+
+            if(_started)
+            {
+                InitialSetup();
+            }
+        }
+
+        ivyToCut = SceneEntities.getNearest(ivyName);
+
+        if( ivyToCut != null )
+        {
+            // We want to cut ivy.
+            // Is the player in the Tree Area
+            if (ivyAreaToUse.contains(Players.getLocal().getLocation()))
+            {
+                // if they are, start chopping ivy
+                CutLogs();
+            }
+            else
+            {
+                // if the player moves out of the area for any reason. bring them back!
+                MovePlayerBackToArea();
+            }
+        }
+    }
+
+    public void InitialSetup()
+    {
+        DetermineTreeAreaAndPathData();
+    }
+
+    public void DetermineTreeAreaAndPathData()
+    {
+        // Create an array of all potential Area's for chopping Ivy
+        Area[] potentialAreas = new Area[]
+        {
+            VarrockCastleArea,
+            FaladorSouthArea
+        };
+
+        // find the one the player is in.
+        for( Area a : potentialAreas )
+        {
+            if( a.contains(Players.getLocal().getLocation()))
+            {
+                ivyAreaToUse = a;
+            }
+        }
+    }
+
+    public void MovePlayerBackToArea()
+    {
+        Tile[] verts = ivyAreaToUse.getAreaVertices();
+        Tile closest = null;
+        double dist = 99999999999d;
+
+        for( Tile t : verts )
+        {
+            if(t.getLocation().distance(Players.getLocal().getLocation()) < dist)
+            {
+                dist = t.getLocation().distance(Players.getLocal().getLocation());
+                closest = t;
+            }
+        }
+
+        if( Walking.walk( closest ) )
+        {
+            // WOOOP I think this means the player is there.
+        }
+    }
+
+    public void CutLogs()
+    {
+        BotUtil.BOTSTATE = BotUtil.BotState.CHOPPING_TREE;
+
+        // Now that we've found a valid Tree to cut.
+        if( ivyToCut != null )
+        {
+            // Tree is valid, so check if its on screen and player is Idle.
+            // Added Player not in combat because in some places lower level players get attacked by highwaymen.
+            if( ivyToCut.isOnScreen() && Players.getLocal().isIdle() && !Players.getLocal().isInCombat() )
+            {
+                ivyToCut.interact(actionToUse, ivyToCut.getDefinition().getName());
+                Time.sleep(Random.nextInt(500, 1000));
+            }
+            else
+            {
+                // if conditions for cutting not met, then turn the camera to face the tree so we can see it.
+                // and hover over it until player is Idle and not in combat.
+
+                if(ivyToCut.getLocation().distance(Players.getLocal().getLocation()) > 2d)
+                {
+                    Camera.turnTo(ivyToCut);
+                    Walking.findPath(ivyToCut);
+                }
+            }
+        }
+        else
+        {
+            BotUtil.WriteMessage("HUGE ERROR HERE!: " + this.getClass().getName() + " Cannot Find the Bank, Contact DamnYouRobo ASAP. CODE 0" );
+        }
+    }
+}
